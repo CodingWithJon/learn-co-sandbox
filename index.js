e61a091a2a5283760c00ae3fbb85119190a490b9
Jonathan Fink <collegepulsemag@gmail.com>
2019-04-26 03:09:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var a=5,b=2,c=3,d='hello',e='goodbye'
+
+var word = "bird"
+
+console.log(word)
+
+word = "dog"
+var message = "hello"
+var firstName = "Jon"
+
+
+console.log(word)
+
+function say(message, firstName) {
+  console.log(`${message},${firstName}!`)
+}
+
+say(message,firstName)
+
+function add(x,y) {
+   var sum = x + y
+   return sum
+}
+
+console.log(add(2,3))
